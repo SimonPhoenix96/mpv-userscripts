0f096b083c8c089b7c7f889911e374c5c46b8d5a
Simon Phoenix <diazjustin1995@yahoo.de>
2020-02-16 20:33:32 +0100

    added comment

--- scripts/wsg-bump-loader/webm-scraper.ps1
+++ scripts/wsg-bump-loader/webm-scraper.ps1
@@ -1,27 +1,27 @@
 #Step 1 - Invoking the web request, making first contact with the site.
 Write-Host "Attempting to call website." -ForegroundColor Green
-$page=Invoke-WebRequest -Uri "https://boards.4channel.org/wsg/thread/3201021" # <-- put [wsg] bump thread link here 
+$page=Invoke-WebRequest -Uri $args[0]
 
 #Step 2 - Create directory for files to be downloaded into
-$wsgDir = "C:\Users\$env:USERNAME\Videos\wsgBumps" # set this to desired directory
-New-Item -Path $wsgDir   -ItemType directory
+$webmDir = $args[1] 
+New-Item -Path $webmDir   -ItemType directory
 Write-Host "Directory has been created." -ForegroundColor Green
 
 #Step 3 - List all webms from page and download them into newly created directory
-$srcrpattern = '((i.4cdn.org)([/|.|\w|\s|-])*\.(?:webm))'
+$srcrpattern = '((i.4cdn.org)([/|.|\w|\s|-])*\.(?:webm))' # change this to download from another page, other than the chan
 $src = ([regex]$srcrpattern ).Matches($page) |  ForEach-Object { $_.Groups[1].Value }
 
 #Step 4 - Download webms, skip if file already exists
 @($src).foreach({
 $fileName = $_ | Split-Path -Leaf
-if (Test-Path("$wsgDir/$fileName")){
+if (Test-Path("$webmDir/$fileName")){
 Write-Host 'Skipping file, already downloaded' -ForegroundColor Yellow
 return
 }
 else 
 {
 Write-Host "Downloading image file $fileName"
-Invoke-WebRequest -Uri $_ -OutFile "$wsgDir/$fileName"
+Invoke-WebRequest -Uri $_ -OutFile "$webmDir/$fileName"
 Write-Host 'Image download complete'
 }
 })
