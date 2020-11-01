135380a60a82b07d6ba4d1aad266900719dee8d0
simonphoenix96 <diazjustin1995@yahoo.de>
2020-11-01 15:56:41 +0100

    merge webm-autoloader-v2 with webm-autoloader

--- webm-autoloader.lua
+++ webm-autoloader.lua
@@ -9,17 +9,17 @@
 --
 --
 -- !!! change this to false if u just want to use availible files in webmDir
-onlineMode = true
+onlineMode = false
 --
 -- !!! change following link to page you want to download webms from
-webPage = 'https://boards.4channel.org/wsg/thread/3530239' -- 'https://www.bumpworthy.com/bumps/' 
+webPage = 'https://boards.4channel.org/wsg/thread/3352336' -- 'https://www.bumpworthy.com/bumps/' 
 --
 -- !!! change following variable to amount of desired webms to be between episodes
 bumpCount = 3
 --
 -- !!! change this to desired webm save directory, on windows seperate path with double backslash, on linux with single forward slash  
 --     Default: 0 || which means webmDir points the MPVs Script Folder
-webmDir = 'F:\\Videos\\bumps\\wsg'
+webmDir = 'F:\\Videos\\bumps\\'
 --
 -- downloads webms off single webPage currently only 4chan thread support
 singlePage = true
@@ -51,47 +51,109 @@ function downloadWebms()
 
 end
 --
+-- number of lines in a file
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
 
--- wm4's modified function
+-- see if the file exists
+function file_exists(file)
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
+  end
+
+  -- TODO: search table for value
+function has_value (tab, val)
+    for index, value in ipairs(tab) do
+      print("check if alreadyPLayedBumps contains: " .. value .. "index: " .. index)  
+      if value == val then
+      print( value  .. " contained in alreadyPLayedBumps") 
+      return true
+      end
+    end
+   return false
+end
+  -- wm4's modified function
 function add_files_at(index, files)
+
+  
 
     index = index - 1
     local oldcount = mp.get_property_number("playlist-count", 1)
+  
+    if #files <= 1 then
+      playlistSize = 1
+      print("playlistsize" .. playlistSize)
+    else
+      playlistSize = #files + (bumpCount  * #files)
+      print("playlistsize" .. playlistSize)
+    end
+  
+  
     
-	if #files <= 1 then 
-	playlistSize = 1 
-	print("playlistsize" .. playlistSize)
-	else 
-	playlistSize = #files + (bumpCount  * #files)
-    print("playlistsize" .. playlistSize)	
-	end
+    for i = 1, playlistSize do
+  
+      
+  
+      -- random number
+      math.randomseed(os.time() * os.time())
+      j = math.random(#bumpFiles)
+  
+  
+  
+  
+      local bumpFileCounter = 1
+      -- add bumps tosimosi playlist
+      while(bumpFileCounter <= bumpCount ) do
+        
+  
+  
+        if(has_value(alreadyPlayedBumpsLines, bumpFiles[j])) then
+          print("removing: " .. bumpFiles[j] .. " from list.") 
+            table.remove(bumpFiles, j)
+            print("Current bumpFiles size == " .. #bumpFiles)
+            print("going to continue mark")
+            goto continue
+          else
 
 
-
-	for i = 1, playlistSize do
-
-  local bumpFileCounter = 1
-
-	math.randomseed(os.time() * os.time())
-	j = math.random(#bumpFiles)
-
-
-	while(bumpFileCounter <= bumpCount ) do
     print("adding " .. bumpFiles[j] .. " to playlist")
-	  mp.commandv("loadfile", bumpFiles[j], "append")
-	  bumpFileCounter = bumpFileCounter + 1
-    print("removing: " .. bumpFiles[j] .. " from list. Current bumpFiles size == " .. #bumpFiles)
+	mp.commandv("loadfile", bumpFiles[j], "append")
+	bumpFileCounter = bumpFileCounter + 1
+    print("removing: " .. bumpFiles[j] .. " from list.") 
     table.remove(bumpFiles, j)
-	end
+    print("Current bumpFiles size == " .. #bumpFiles)
+        -- appends played filename to the last line of alreadyPlayedBumps.txt
+        print("adding " .. bumpFiles[j] .. " to alreadyPlayedBumps.txt")
+        -- Opens a file in append mode
 
-bumpFileCounter = 1
-print("bumpFileCounter " .. bumpFileCounter)
+        writeFile = io.open("alreadyPlayedBumps.txt", "a")
+        io.output(writeFile)
+        io.write(bumpFiles[j] .. "\n" ) 
+        -- closes the open file
+        io.close(writeFile)
+      end
+
+      ::continue::
 
 
-      print("adding " .. files[i] .. " to playlist")
-      mp.commandv("loadfile", files[i], "append")
-      mp.commandv("playlist-move", oldcount + i - bumpCount, index + i - bumpCount)
+    end
 
+	
+    print("bumpFileCounter " .. bumpFileCounter)
+    bumpFileCounter = 1
+    
+
+
+    print("adding " .. files[i] .. " to playlist")
+    mp.commandv("loadfile", files[i], "append")
+    mp.commandv("playlist-move", oldcount + i - bumpCount, index + i - bumpCount)
 
     end
 end
@@ -104,21 +166,6 @@ function script_path()
 end
 --
 
-
--- -- Shuffle bumpFiles
--- function shuffle(t)
-  -- local tbl = {}
-  -- for i = 1, #t do
-    -- tbl[i] = t[i]
-  -- end
-  -- for i = #tbl, 2, -1 do
-	-- math.randomseed(os.time())
-	-- local j = math.random(i)
-    -- tbl[i], tbl[j] = tbl[j], tbl[i]
-  -- end
-  -- return tbl
--- end
---
 
 -- from here modified wm4 stuff
 MAXENTRIES = 5000
@@ -221,6 +268,7 @@ end
 local autoloaded = nil
 
 function find_and_add_entries()
+    
     local path = mp.get_property("path", "")
 
     local dir, filename = utils.split_path(path)
@@ -250,7 +298,8 @@ function find_and_add_entries()
 
 	-- read wsg folders content aswell
     bumpFiles = utils.readdir(webmDir)
-    -- filter aswel
+
+    -- filter
     table.filter(bumpFiles, function (v, k)
         if string.match(v, "^%.") then
             return false
@@ -268,17 +317,6 @@ function find_and_add_entries()
         return
     end
 
-    table.filter(files, function (v, k)
-        if string.match(v, "^%.") then
-            return false
-        end
-        local ext = get_extension(v)
-        if ext == nil then
-            return false
-        end
-        return EXTENSIONS[string.lower(ext)]
-    end)
-
     -- randomize bumpFiles order of elements
     -- shuffle(bumpFiles)
     -- &
@@ -294,8 +332,6 @@ function find_and_add_entries()
 	end
 	--
 
-
-
     table.sort(files, alnumcomp)
 
     if dir == "." then
@@ -304,6 +340,7 @@ function find_and_add_entries()
 
     -- Find the current pl entry (dir+"/"+filename) in the sorted dir list
     local current
+
     for i = 1, #files do
         if files[i] == filename then
             current = i
@@ -344,6 +381,19 @@ function find_and_add_entries()
             end
         end
     end
+-- remove alreadyPlayedBumps.txt when same amount as existing bump files as this would mean we've played them all 
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+alreadyPlayedBumpsLines = lines_from("alreadyPlayedBumps.txt")
+
+print("Nr. of Bumps in bumpFolder: " .. #bumpFiles .. "Nr. of already played Bumps: " .. #alreadyPlayedBumpsLines)
+if(#bumpFiles == #alreadyPlayedBumpsLines)
+then
+  os.remove ("alreadyPlayedBumps.txt")
+end
+
+
 
     add_files_at(pl_current + 1, append[1])
     add_files_at(pl_current, append[-1])
@@ -351,4 +401,5 @@ function find_and_add_entries()
 end
 
 if(onlineMode) then mp.register_event("start-file", downloadWebms) end
+
 mp.register_event("start-file", find_and_add_entries)
