abc5332693bcb6cdea2061e90e9eface4cd55289
simonphoenix96 <diazjustin1995@yahoo.de>
2021-03-21 02:25:36 +0100

    remove print calls

--- webm-bump-loader/webm-autoloader.lua
+++ webm-bump-loader/webm-autoloader.lua
@@ -19,7 +19,7 @@ bumpCount = 3
 --
 --
 -- bumpworthy.com support aka. adult swim bumps
-bumpWorthy = false
+bumpWorthy = true
 --
 -- !!! change this to desired webm save directory, on windows seperate path with double backslash, on linux with single forward slash
 --     Default: 0 || which means webmDir points the MPVs Script Folder
@@ -66,11 +66,11 @@ function downloadWebms()
       then
          -- Windows Version
          if(bumpWorthy) then
-            print('powershell.exe  -file "' .. script_path() .. 'webm-scraper.ps1" "' .. "bumpworthy" .. '" "' .. webmDir .. '" ' .. tostring(streamMode))
+            -- print('powershell.exe  -file "' .. script_path() .. 'webm-scraper.ps1" "' .. "bumpworthy" .. '" "' .. webmDir .. '" ' .. tostring(streamMode))
             os.execute('powershell.exe -file "' .. script_path() .. 'webm-scraper.ps1" "bumpworthy" "' .. webmDir .. '" ' .. tostring(streamMode))
          else
-            print('powershell.exe -file "' .. script_path() .. 'webm-scraper.ps1" "' .. "4chan" .. '" "' .. webmDir .. '" ' .. tostring(streamMode))
-            -- print('if(((Get-Process mpv -ErrorAction SilentlyContinue) -eq $null)){powershell.exe -windowstyle hidden -file "' .. script_path() .. 'webm-scraper.ps1" "' .. "4chan" .. '" "' .. webmDir .. '" ' .. tostring(streamMode) .. "}" )
+            -- print('powershell.exe -file "' .. script_path() .. 'webm-scraper.ps1" "' .. "4chan" .. '" "' .. webmDir .. '" ' .. tostring(streamMode))
+            -- -- print('if(((Get-Process mpv -ErrorAction SilentlyContinue) -eq $null)){powershell.exe -windowstyle hidden -file "' .. script_path() .. 'webm-scraper.ps1" "' .. "4chan" .. '" "' .. webmDir .. '" ' .. tostring(streamMode) .. "}" )
             -- os.execute('if(-not((Get-Process mpv -ErrorAction SilentlyContinue) -eq $null)){powershell.exe -windowstyle hidden -file "' .. script_path() .. 'webm-scraper.ps1" "' .. "4chan" .. '" "' .. webmDir .. '" ' .. tostring(streamMode) .. "}" ) -- change regex pattern in webm-scraper.ps1 to website other than the chan
             os.execute('powershell.exe -file "' .. script_path() .. 'webm-scraper.ps1" "' .. "4chan" .. '" "' .. webmDir .. '" ' .. tostring(streamMode))
          end
@@ -89,7 +89,7 @@ end
 -- get script path
 function script_path()
    local script_path = debug.getinfo(1, "S").source
-   -- print("debug_info " .. script_path:match("(.*/)"))
+   -- -- print("debug_info " .. script_path:match("(.*/)"))
    return script_path:match("(.*/)")
 end
 --
@@ -114,9 +114,9 @@ end
 -- APB: check if table contains value
 function has_value (tab, val)
    for index, value in ipairs(tab) do
-      print("comparing " .. value .. #value ..  ' ' .. val .. #val .. ' ' .. tostring(value == val) )
+      -- print("comparing " .. value .. #value ..  ' ' .. val .. #val .. ' ' .. tostring(value == val) )
       if value == val then
-         print("table has value " .. value ..  ' ' .. val )
+         -- print("table has value " .. value ..  ' ' .. val )
          return true
       end
    end
@@ -149,7 +149,7 @@ function add_files_at(index, files)
    index = index  - 1
    
    local oldcount = mp.get_property_number("playlist-count", 1)
-   print("number of episodes" .. #files)
+   -- -- print("number of episodes" .. #files)
    
    -- no bumps at end
    -- for i = 1, #files do
@@ -160,9 +160,9 @@ function add_files_at(index, files)
       -- APB
       while(bumpFileCounter <= bumpCount ) do
          
-         print('number of bump files xxx: '.. #bumpFiles)
-         print("Current bumpFiles size == " .. #bumpFiles)
-         -- print("bumpFilesCopy Size: " .. #bumpFilesCopy)
+         -- -- print('number of bump files xxx: '.. #bumpFiles)
+         -- -- print("Current bumpFiles size == " .. #bumpFiles)
+         -- -- print("bumpFilesCopy Size: " .. #bumpFilesCopy)
          math.randomseed(os.time() * os.time())
          j = math.random(#bumpFiles)
          -- remove carriage return symbol
@@ -171,25 +171,25 @@ function add_files_at(index, files)
          bumpFileWithoutNR = string.gsub(bumpFileWithoutNR, "%s+", "")
 
 
-         -- print("no. bump already played? " .. #alreadyPlayedBumpsLines)
-         -- print("bump already played? " .. tostring(has_value(alreadyPlayedBumpsLines, bumpFileWithoutNR)))
+         -- -- print("no. bump already played? " .. #alreadyPlayedBumpsLines)
+         -- -- print("bump already played? " .. tostring(has_value(alreadyPlayedBumpsLines, bumpFileWithoutNR)))
          if(has_value(alreadyPlayedBumpsLines, bumpFileWithoutNR)) then
-            print("bump already played removing: " .. bumpFileWithoutNR .. " from list.")
+            -- -- print("bump already played removing: " .. bumpFileWithoutNR .. " from list.")
             table.remove(bumpFiles, j)
-            print("Current bumpFiles size == " .. #bumpFiles)
-            print("going to continue mark")
+            -- -- print("Current bumpFiles size == " .. #bumpFiles)
+            -- -- print("going to continue mark")
             -- goto continue
 
          else
 
 
             -- add bump to playlist between episodes
-            print("appending bump " .. bumpFileWithoutNR .. " to playlist")
+            -- -- print("appending bump " .. bumpFileWithoutNR .. " to playlist")
             mp.commandv("loadfile",  bumpFileWithoutNR, "append")
 
             bumpFileCounter = bumpFileCounter + 1
 
-            print("removing: " .. bumpFileWithoutNR .. " from list.")
+            -- -- print("removing: " .. bumpFileWithoutNR .. " from list.")
             table.remove(bumpFiles, j)
 
 
@@ -199,17 +199,17 @@ function add_files_at(index, files)
       end
 
       -- APB
-      print("bumpFileCounter " .. bumpFileCounter)
+      -- -- print("bumpFileCounter " .. bumpFileCounter)
 
       -- add episode to playlist between bumps
       
       if (i <= #files) then
-         print("episodes added: " .. i)
-         print("adding episode " .. files[i] .. " to playlist index: " .. index)
+         -- -- print("episodes added: " .. i)
+         -- -- print("adding episode " .. files[i] .. " to playlist index: " .. index)
          mp.commandv("loadfile", files[i], "append")
       end 
-      print("oldcount " .. oldcount)
-      print("indschmex: " .. index)
+      -- -- print("oldcount " .. oldcount)
+      -- -- print("indschmex: " .. index)
       mp.commandv("playlist-move", oldcount + i - bumpCount, index + i - bumpCount)
       
    end
@@ -367,7 +367,7 @@ function find_and_add_entries()
 
    --    -- go through bumpFiles
    -- for i = 1, #bumpFiles do
-   --    print("file in bumpFiles: " .. bumpFiles[i])
+   --    -- print("file in bumpFiles: " .. bumpFiles[i])
    -- end
 
    -- wm4
@@ -457,11 +457,11 @@ function find_and_add_entries()
 
    -- APB
    numNeededBumpFiles = (#append[1] * bumpCount) + bumpCount
-   print("number of bump files: " .. #bumpFiles .." needed bump files: " .. numNeededBumpFiles) 
-   print("Nr. of Bumps in bumpFiles: " .. #bumpFiles .. "Nr. of already played Bumps: " .. #alreadyPlayedBumpsLines)
+   -- print("number of bump files: " .. #bumpFiles .." needed bump files: " .. numNeededBumpFiles) 
+   -- print("Nr. of Bumps in bumpFiles: " .. #bumpFiles .. "Nr. of already played Bumps: " .. #alreadyPlayedBumpsLines)
    if((#alreadyPlayedBumpsLines + numNeededBumpFiles > #bumpFiles ) == true)
    then
-      print("reset already played bumps!")
+      -- print("reset already played bumps!")
       io.open(alreadyPlayedBumpsPath,"w"):close()
    end
    
@@ -474,7 +474,7 @@ function find_and_add_entries()
    
    -- -- download bumps from previous thread if not enough bumps
    
-   -- print("number of bump files: " .. #bumpFiles .." needed bump files: " .. numNeededBumpFiles) 
+   -- -- print("number of bump files: " .. #bumpFiles .." needed bump files: " .. numNeededBumpFiles) 
    -- -- incase not enough bumpFiles to put between episodes use bumpFilesCopy to re-add same bumps between episodes
    -- bumpFilesCopy = shallowcopy(bumpFiles)
    
@@ -486,11 +486,11 @@ function find_and_add_entries()
    --       table.insert(bumpFiles, bumpFilesCopy[j])
    --    end
    -- end
-   -- print("number of bump files: " .. #bumpFiles)
+   -- -- print("number of bump files: " .. #bumpFiles)
    
-   print("adding files onward from played file")
+   -- print("adding files onward from played file")
    add_files_at(pl_current + 1, append[1])
-   -- print("adding files backward from played file")
+   -- -- print("adding files backward from played file")
    -- add_files_at(pl_current, append[-1]) 
 
    -- for some unholy reason find_and_add_entries goes into an endless loop if i dont unregister it
@@ -524,19 +524,19 @@ end
 --  Event Loop
 function loading_next_playlist_file()
    local current_playlist_pos = tonumber(mp.get_property('playlist-current-pos'))
-   print ( "current playlist pos : " .. tostring(current_playlist_pos))
+   -- print ( "current playlist pos : " .. tostring(current_playlist_pos))
    if addedToPlayedBumps == bumpCount then
-      print("resetting addedtoplayedbumps")
+      -- print("resetting addedtoplayedbumps")
       addedToPlayedBumps = 0 
       resetAddedToPlayedBumps = true
    end
    
    if (current_playlist_pos >= 1  and addedToPlayedBumps ~= bumpCount and  resetAddedToPlayedBumps ~= true) then
       local current_playlist_filename =  mp.get_property('playlist/' .. current_playlist_pos.. '/filename')
-      print("Loading next playlist file " .. current_playlist_filename .. " at playlist index " ..  current_playlist_pos)
-      -- print("Current bumpFiles size == " .. #bumpFiles)
+      -- print("Loading next playlist file " .. current_playlist_filename .. " at playlist index " ..  current_playlist_pos)
+      -- -- print("Current bumpFiles size == " .. #bumpFiles)
       -- appends played filename to the last line of alreadyPlayedBumps.txt
-      print("adding " .. current_playlist_filename .. " to " .. alreadyPlayedBumpsPath)
+      -- print("adding " .. current_playlist_filename .. " to " .. alreadyPlayedBumpsPath)
 
       -- Opens a file in append mode
       if (streamMode) then 
@@ -548,7 +548,7 @@ function loading_next_playlist_file()
          -- closes the open file
          io.close(writeFile)
          addedToPlayedBumps = addedToPlayedBumps + 1
-         print("addedtoplayedbumps " .. addedToPlayedBumps)
+         -- print("addedtoplayedbumps " .. addedToPlayedBumps)
          end
       else
          writeFile = io.open(alreadyPlayedBumpsPath, "a")
@@ -557,7 +557,7 @@ function loading_next_playlist_file()
          -- closes the open file
          io.close(writeFile)
          addedToPlayedBumps = addedToPlayedBumps + 1
-         print("addedtoplayedbumps " .. addedToPlayedBumps)
+         -- print("addedtoplayedbumps " .. addedToPlayedBumps)
       
       end
    end
@@ -569,7 +569,7 @@ end
 
 function loop_append_to_apb()
    -- mp.add_key_binding("ctrl+a", "loop_append_to_apb", loop_append_to_apb)
-   print("loop_append_to_apb triggered")
+   -- print("loop_append_to_apb triggered")
    mp.register_event("file-loaded", loading_next_playlist_file)
 end
 
